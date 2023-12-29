9f2e3143dd114a166f48d3ad89602130349ea8b1
Stephen Meehan <stephenmee@gmail.com>
2023-12-29 23:21:00 +0000

    capstone week 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import './App.css';
+import './App.css'
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import About from './components/About'
+import Menu from './components/Menu'
+import Reservations from './components/Reservations'
+import OrderOnline from './components/OrderOnline'
+import Login from './components/Login'
+import Main from './components/Main'
+import Footer from './components/Footer'
 
 function App() {
-  return (
-    <div className="App">
-Homepage
-    </div>
-  );
+    return (
+        <>
+            <Header className="App-header" />
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/menu" element={<Menu />} />
+                <Route path="/reservations" element={<Reservations />} />
+                <Route path="/order-online" element={<OrderOnline />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+            <Main />
+            <Footer className="Footer" />
+        </>
+    )
 }
 
-export default App;
+export default App
---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <main className="main">
+            <div>Main Page Content</div>
+        </main>
+    )
+}
+
+export default Main
